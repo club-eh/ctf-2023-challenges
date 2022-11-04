5b1eb83f0137751473253e9b9e133894e60dc31b
sudoBash418 <sudoBash418@gmail.com>
2022-11-03 22:39:14 -0600

    rusty-rev-0: Refactor encode_flag.py

--- rusty-rev-0/source/encode_flag.py
+++ rusty-rev-0/source/encode_flag.py
@@ -2,30 +2,58 @@
 
 """Encodes the given flag into the source code of `libverify`."""
 
-from io import StringIO
-import argparse
+from typing import Literal
+import io
+
+
+class RustConstants:
+	"""Utility class for easily writing Rust constants."""
+
+	# Rust integer types
+	TYPES_UINT = Literal["u8", "u16", "u32", "u64", "usize"]
+	TYPES_SINT = Literal["i8", "i16", "i32", "i64", "isize"]
+	TYPES_INT = Literal[TYPES_UINT, TYPES_SINT]
+
+	def __init__(self, prefix: str = "pub(crate) ") -> None:
+		self.prefix = prefix
+		self.sio = io.StringIO()
+
+	def get_value(self) -> str:
+		return self.sio.getvalue()
+
+	def write(self, data: str):
+		"""Write an arbitrary string."""
+		self.sio.write(data)
+
+	def write_int(self, name: str, type: TYPES_INT, value: int):
+		"""Write the given integer with the given type."""
+		self.sio.write(self.prefix)
+		self.sio.write(f"const {name}: {type} = {value};\n")
+
+	def write_u8_array(self, name: str, data: bytes):
+		"""Write the given byte array as an array of u8 integers."""
+		self.sio.write(self.prefix)
+		self.sio.write(f"const {name}: [u8; {len(data)}] = {list(data)!r};\n")
 
 
 XOR_KEY = 0b01011001
 
 
 def encode(flag: bytes) -> str:
-	sio = StringIO()
+	rc = RustConstants()
 
-	# flag length
-	sio.write(f"pub(crate) const FLAG_LEN: usize = {len(flag)};\n")
+	rc.write_int("FLAG_LEN", "usize", len(flag))
 
-	# xor key
-	sio.write(f"pub(crate) const XOR_KEY: u8 = {XOR_KEY};\n")
+	rc.write_int("XOR_KEY", "u8", XOR_KEY)
 
-	# xor encoded flag
-	encoded_flag = bytes(c ^ XOR_KEY for c in flag)
-	sio.write("pub(crate) const ENCODED_FLAG: [u8; FLAG_LEN] = {!r};\n".format(list(encoded_flag)))
+	rc.write_u8_array("ENCODED_FLAG", bytes(c ^ XOR_KEY for c in flag))
 
-	return sio.getvalue()
+	return rc.get_value()
 
 
 if __name__ == "__main__":
+	import argparse
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument("flag", action="store")
 	parser.add_argument("output_file", action="store")
