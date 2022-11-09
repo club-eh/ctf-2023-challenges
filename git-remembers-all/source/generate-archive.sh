1e34d566766aba517739ed349ceae5ce9e479ac3
sudoBash418 <sudoBash418@gmail.com>
2022-11-08 23:22:19 -0700

    git-remembers-all: Add source/readme and reformat generate-archive.sh

--- git-remembers-all/source/generate-archive.sh
+++ git-remembers-all/source/generate-archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# constructs the git repo from assets/*
+# constructs and archives the git repo
 
 set -e
 
@@ -30,75 +30,66 @@ if [[ -e "$WORKDIR" ]]; then
 fi
 mkdir -p "$WORKDIR"/repo-{local,remote}
 
-(
-	cd "$WORKDIR"/repo-local
 
-	## initialize local repository
-	git init -b $GIT_BRANCH_NAME
+cd "$WORKDIR"/repo-local
+
+## initialize local repository
+git init -b $GIT_BRANCH_NAME
+
+## create initial commit with readme
+cp "$SRCDIR"/README.md ./
+
+git add README.md
+GIT_AUTHOR_DATE="2023-03-14 01:29:22" GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" \
+	git commit README.md -m "Initial commit"
+
+## actually add some project files
+poetry init --no-interaction \
+	--author="$AUTHOR_NAME <${AUTHOR_EMAIL}>" \
+	--description="Lorem Ipsum web app" \
+	--dependency=starlette \
+	--dependency=uvicorn
+
+echo "poetry.lock" > .gitignore
+echo ".venv" >> .gitignore
+
+cp "$SRCDIR"/app-v1.py ./app.py
+
+git add app.py pyproject.toml .gitignore
+GIT_AUTHOR_DATE="2023-03-14 01:35:17" GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" \
+	git commit app.py pyproject.toml .gitignore -m "Add initial project files"
+
+## add session middleware with leaked secret
+poetry add --lock itsdangerous
+
+cp "$SRCDIR"/app-v2.py ./app.py
+
+# write b64-encoded secret to .env
+echo -n "SECRET_KEY=" > .env
+echo "$FLAG" | base64 >> .env
+
+git add .env
+GIT_AUTHOR_DATE="2023-03-14 01:47:50" GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" \
+	git commit app.py pyproject.toml .env -m "Add session middleware for security"
 
 
-	## create initial commit with readme
-	cp "$SRCDIR"/README.md ./
+## initialize remote repository
+git -C "$WORKDIR"/repo-remote init -b main --bare
 
-	git add README.md
-	GIT_AUTHOR_DATE="2023-03-14 01:29:22" GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" \
-		git commit README.md -m "Initial commit"
 
-	## actually add some project files
-	poetry init --no-interaction \
-		--author="$AUTHOR_NAME <${AUTHOR_EMAIL}>" \
-		--description="Lorem Ipsum web app" \
-		--dependency=starlette \
-		--dependency=uvicorn
+## push local changes to remote repo
+git remote add github ../repo-remote
+git push --set-upstream github $GIT_BRANCH_NAME
 
-	echo "poetry.lock" > .gitignore
-	echo ".venv" >> .gitignore
+## the "oh shit", "fix up before anyone notices" moments
+echo ".env" >> .gitignore
 
-	cp "$SRCDIR"/app-v1.py ./app.py
+git add .gitignore
+git rm .env
+GIT_COMMITTER_DATE="2023-03-14 01:55:51" git commit --amend -C HEAD
+git push --force
 
-	git add app.py pyproject.toml .gitignore
-	GIT_AUTHOR_DATE="2023-03-14 01:35:17" GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" \
-		git commit app.py pyproject.toml .gitignore -m "Add initial project files"
 
-	## add session middleware with leaked secret
-	poetry add --lock itsdangerous
-
-	cp "$SRCDIR"/app-v2.py ./app.py
-
-	# write b64-encoded secret to .env
-	echo -n "SECRET_KEY=" > .env
-	echo "$FLAG" | base64 >> .env
-
-	git add .env
-	GIT_AUTHOR_DATE="2023-03-14 01:47:50" GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" \
-		git commit app.py pyproject.toml .env -m "Add session middleware for security"
-)
-
-(
-	## initialize remote repository
-	cd "$WORKDIR"/repo-remote
-	git init -b main --bare
-)
-
-(
-	cd "$WORKDIR"/repo-local
-
-	## push local changes to remote repo
-	git remote add github ../repo-remote
-	git push --set-upstream github $GIT_BRANCH_NAME
-
-	## the "oh shit", "fix up before anyone notices" moments
-	echo ".env" >> .gitignore
-
-	git add .gitignore
-	git rm .env
-	GIT_COMMITTER_DATE="2023-03-14 01:55:51" git commit --amend -C HEAD
-	git push --force
-)
-
-(
-	cd "$WORKDIR"/repo-remote
-
-	## archive the resulting remote repo
-	tar cf "$OUTFILE" * --mtime="2023-03-14 04:31:23" --owner=0 --group=0
-)
+## archive the resulting remote repo
+cd "$WORKDIR"/repo-remote
+tar cf "$OUTFILE" * --mtime="2023-03-14 04:31:23" --owner=0 --group=0
---
+++ git-remembers-all/source/readme.txt
@@ -0,0 +1,3 @@
+This directory contains the source files used to generate ../static/git-repo.tar
+
+Run `./generate-archive.sh` to regenerate ../static/git-repo.tar
