a913c2d3ecbf517f41a6a88a6667ad5f84af31b9
Nemi Rai <rain3@mymacewan.ca>
2023-01-12 05:30:27 -0700

    Botanic Market 0: remove login

--- botanic-market-0/dynamic/www/index.php
+++ botanic-market-0/dynamic/www/index.php
@@ -1,5 +1,4 @@
 <?php
-	// Set admin to false
 	include 'src/setCookies.php';
 ?>
 <!DOCTYPE HTML>
@@ -12,7 +11,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-	<nav class="navbar" style="background-color: #C19A4D;">
+	<nav class="navbar" style="background-color: #315527;">
 		<div class="container-fluid">
     		<span class="navbar-brand mb-0 h1" style="color: #000000">Botanic Market</span>
 		</div>
@@ -25,25 +24,10 @@
 			<div class="col-6 pt-3 text-center">
 				<img class="banner" src="img/banner.png"></img>
 			</div>
-			<div class="col-3 p-2">
-				<div class="mb-3">
-					<label for="user" class="form-label">Username</label>
-					<input type="text" class="form-control-sm" id="user" placeholder="Enter your username">
-				</div>
-				<div class="mb-3">
-					<label for="pass" class="form-label">Password</label>
-					<input type="password" class="form-control-sm"  style="max-width: 100%;" id="pass" placeholder="Enter your password">
-				</div>
-				<div class="text-right">
-					<button type="button" data-toggle="button" class="btn-sm btn-light ms-2">Login</button>
-					<button type="button" data-toggle="button" class="btn-sm btn-light" disabled>Register</button>
-				</div>
-			</div>
 		</div>
 		<hr style="color: #FFFFFF;">
 
 		<?php
-			// Check for admin cookie
 			include 'src/checkRole.php';
 		?>
 	
