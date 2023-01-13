f1409db487efd4aa3b28d109a7dbf563b75e7554
Nemi Rai <rain3@mymacewan.ca>
2023-01-13 13:31:25 -0700

    Wheely easy: Add easy crypto challenge

---
+++ wheely-easy/static/src/spin.js
@@ -0,0 +1,17 @@
+function rotateMiddle() {
+	key1 = document.querySelector("#key1").value;
+	angle = (key1 % 36) * -10;
+	document.querySelector("#middle").style.transform = `rotate(${angle}deg)`;
+	
+	if (document.querySelector('#key2').value == "" || document.querySelector('#key2').value == "0") {
+		document.querySelector("#inner").style.transform = `rotate(${angle}deg)`;
+	}
+}
+function rotateInner() {
+	key1 = document.querySelector("#key1").value;
+	key2 = document.querySelector("#key2").value;
+	angle1 = ((key1) % 36) * -10;
+	angle2 = angle1 + (((key2) % 36) * -10);
+	document.querySelector("#inner").style.transform = `rotate(${angle2}deg)`;
+}
+
