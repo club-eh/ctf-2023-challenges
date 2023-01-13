31d0a9842e8f72202edb771e477bd172dcfce30f
sudoBash418 <sudoBash418@gmail.com>
2023-01-13 16:13:05 -0700

    rusty-rev-2: Add support for building arm64 binaries
    Also, add a note regarding arm64 solve time.

--- rusty-rev-2/source/build.sh
+++ rusty-rev-2/source/build.sh
@@ -42,16 +42,17 @@ echo "  => Stripping flag-checker..."
 	cd out/
 	# remove debug info (but leave symbols)
 	strip --strip-debug flag-checker
+	aarch64-linux-gnu-strip --strip-debug flag-checker-arm64
 	# remove Rust stdlib symbols
 	#readelf -sW flag-checker | grep -E '_ZN3std\w+' -o | xargs -i strip flag-checker -N {}
 )
 
 echo "  => Installing flag-checker..."
 (
-	install -pm755 -t ../static ./out/flag-checker
+	install -pm755 -t ../static ./out/flag-checker{,-arm64}
 )
 
 echo "  => Done"
 (
-	ls -l --color ./out/flag-checker
+	ls -l --color ./out/flag-checker{,-arm64}
 )
--- rusty-rev-2/source/libverify/build.sh
+++ rusty-rev-2/source/libverify/build.sh
@@ -9,7 +9,9 @@ OUTDIR="../out"
 
 
 RUSTFLAGS="-C target-cpu=x86-64-v2" cargo build --release
+cargo build --release --target=aarch64-unknown-linux-gnu
 
-install -pm 644 -t "$OUTDIR" ./target/release/libverify.a
+install -pm 644 ./target/release/libverify.a "${OUTDIR}/libverify.a"
+install -pm 644 ./target/aarch64-unknown-linux-gnu/release/libverify.a "${OUTDIR}/libverify-arm64.a"
 
 cbindgen --quiet --lang C -o "$OUTDIR"/libverify.h
